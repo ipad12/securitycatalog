590f1d9e08fb51b1811ce6965c368b8e4eeebb10
ipad12 <iking.padilla12@gmail.com>
2020-06-24 13:53:11 +0800

    Add files via upload

--- security/admin.py
+++ security/admin.py
@@ -1,8 +1,73 @@
 from django.contrib import admin
 
-from .models import Domain, Technology, Product
+from .models import Domain, Technology, Product, ProdSlideDeck, ScopeDoc, POCSOW, Datasheet, CompetitiveInfo, Vendor
 
-admin.site.register(Domain)
-admin.site.register(Technology)
-admin.site.register(Product)
 
+from django.contrib.admin.models import LogEntry
+
+class PSDInLine(admin.StackedInline):
+    model = ProdSlideDeck
+    extra=0
+    
+class SDInLine(admin.StackedInline):
+    model = ScopeDoc
+    extra=0
+    
+class POCSOWInLine(admin.StackedInline):
+    model = POCSOW
+    extra=0
+    
+class DSInLine(admin.StackedInline):
+    model = Datasheet
+    extra=0
+    
+class CIInLine(admin.StackedInline):
+    model = CompetitiveInfo
+    extra=0
+    
+class ProductAdmin(admin.ModelAdmin):    
+    inlines = [
+        PSDInLine,
+        SDInLine,
+        POCSOWInLine,
+        DSInLine,
+        CIInLine,
+    ]
+    save_on_top=True
+    list_display = ('name','vendor', 'technologies')
+    list_filter = ['vendor']
+    filter_horizontal = ['technology']
+    
+    def technologies(self, product):
+        return ",\n".join([p.name for p in product.technology.all()])
+    
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'domain', 'description')
+    list_filter = ['domain']
+    
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('name', '_id')
+
+class LogEntryAdmin(admin.ModelAdmin):
+    readonly_fields = ('content_type',
+        'user',
+        'action_time',
+        'object_id',
+        'object_repr',
+        'action_flag',
+        'change_message'
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_actions(self, request):
+        actions = super(LogEntryAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+
+admin.site.register(LogEntry, LogEntryAdmin)
+admin.site.register(Technology, TechnologyAdmin)    
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Domain, DomainAdmin)
+admin.site.register(Vendor)
--- security/models.py
+++ security/models.py
@@ -1,31 +1,70 @@
 from djongo import models
 
 class Domain(models.Model):
-	
     _id = models.ObjectIdField()
     name = models.CharField(max_length=200)
+    id = models.IntegerField(default="0")
     def __str__(self):
         return self.name	
 
 class Technology(models.Model):
-    
     domain = models.ForeignKey(Domain, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200)
-    description = models.TextField(blank = True, max_length = 128)
-    
+    description = models.TextField(blank = True, max_length=1000)
+        
     def __str__(self):
        	return self.name
 
-class Product(models.Model):
-	
+class Vendor(models.Model):
+    name = models.CharField(max_length=200, default=" ")
+    
+    def __str__(self):
+        return self.name
+
+class Solution(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, null=True)
     technology = models.ForeignKey(Technology, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200, default=" ")
     
-    product_slide_deck = models.FileField(upload_to='slidedeck/', null=True, blank=True)
-    scoping_document = models.FileField(upload_to='scopingdoc/', null=True, blank=True)
-    POC_SOW = models.FileField(upload_to='POC_SOW/', null=True, blank=True)
-    competitive_information = models.FileField(upload_to='competitiveinfo/', null=True, blank=True)
-    datasheet = models.FileField(upload_to='datasheet/', null=True, blank=True)
-	
     def __str__(self):
         return self.name
+    
+class Product(models.Model):
+    technology = models.ManyToManyField(Technology)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=200, default=" ")
+    logoURL = models.CharField(blank = True, max_length=255, null=True)
+
+    def __str__(self):
+        return self.name
+    
+    
+class ProdSlideDeck(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    #solution = models.ForeignKey(Solution, on_delete=models.CASCADE, null=True) 
+    name = models.CharField(max_length=200, default=" ")
+    prodslidedeck = models.CharField(max_length=255)
+    
+class ScopeDoc(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    #solution = models.ForeignKey(Solution, on_delete=models.CASCADE, null=True) 
+    name = models.CharField(max_length=200, default=" ")
+    scopedoc = models.CharField(max_length=255)
+    
+class POCSOW(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    #solution = models.ForeignKey(Solution, on_delete=models.CASCADE, null=True) 
+    name = models.CharField(max_length=200, default=" ")
+    pocsow = models.CharField(max_length=255)
+    
+class Datasheet(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    #solution = models.ForeignKey(Solution, on_delete=models.CASCADE, null=True) 
+    name = models.CharField(max_length=200, default=" ")
+    datasheet = models.CharField(max_length=255)
+    
+class CompetitiveInfo(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    #solution = models.ForeignKey(Solution, on_delete=models.CASCADE, null=True) 
+    name = models.CharField(max_length=200, default=" ")
+    competitiveinfo = models.CharField(max_length=255)
